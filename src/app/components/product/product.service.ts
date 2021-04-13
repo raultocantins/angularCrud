3b5b3809076bd982012846e005f05ea16da775eb
Alex raul <alexbraul.ar@gmail.com>
2021-04-13 16:29:32 -0300

    Ending crud

--- src/app/components/product/product.service.ts
+++ src/app/components/product/product.service.ts
@@ -2,7 +2,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Observable } from 'rxjs';
+import { EMPTY, Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { Product } from './product.model';
 @Injectable({
   providedIn: 'root'
@@ -10,30 +11,57 @@ import { Product } from './product.model';
 export class ProductService {
   baseUrl: string = "http://localhost:3001/products"
   constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
-  showMessage(msg: string): void {
+  showMessage(msg: string, isError: boolean = false): void {
     this.snackBar.open(msg, 'fechar', {
       horizontalPosition: 'right',
       verticalPosition: 'top',
-      duration: 3000
+      duration: 3000,
+      panelClass: isError ? ['msg-error'] : ['msg-success']
     })
   }
   create(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.baseUrl, product);
-  }
-  read():Observable<Product[]>{
+    return this.http.post<Product>(this.baseUrl, product).pipe(
+      map((obj) => obj),
+      catchError(e => this.handleError(e))
 
-    return this.http.get<Product[]>(this.baseUrl)
+    )
   }
-readById(id:string|number):Observable<Product>{
-  const url=`${this.baseUrl}/${id}`
-  return this.http.get<Product>(url)
-}
-update(product:Product): Observable<Product>{
-  const url:string=`${this.baseUrl}/${product.id}`
-  return this.http.put<Product>(url,product)
-}
-delete(id:string|number): Observable<Product>{
-  const url=`${this.baseUrl}/${id}`
-return this.http.delete<Product>(url)
-}
+
+
+  handleError(error: any): Observable<any> {
+    this.showMessage('Ocorreu um erro !', true)
+    return EMPTY
+  }
+  read(): Observable<Product[]> {
+
+    return this.http.get<Product[]>(this.baseUrl).pipe(
+      map((obj) => obj),
+      catchError(e => this.handleError(e))
+
+    )
+  }
+  readById(id: string | number): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.get<Product>(url).pipe(
+      map((obj) => obj),
+      catchError(e => this.handleError(e))
+
+    )
+  }
+  update(product: Product): Observable<Product> {
+    const url: string = `${this.baseUrl}/${product.id}`
+    return this.http.put<Product>(url, product).pipe(
+      map((obj) => obj),
+      catchError(e => this.handleError(e))
+
+    )
+  }
+  delete(id: string | number): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.delete<Product>(url).pipe(
+      map((obj) => obj),
+      catchError(e => this.handleError(e))
+
+    )
+  }
 }
--- src/app/views/product-crud/product-crud.component.ts
+++ src/app/views/product-crud/product-crud.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router'
+import { HeaderService } from 'src/app/components/template/header/header.service';
 @Component({
   selector: 'app-product-crud',
   templateUrl: './product-crud.component.html',
@@ -7,7 +8,15 @@ import {Router} from '@angular/router'
 })
 export class ProductCrudComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor(private router:Router,private headerService:HeaderService) { 
+
+    headerService.setHeaderData={
+      title:"Cadastro de Produto",
+      icon:"storefront",
+      routeUrl:'/products'
+    }
+
+  }
 
   ngOnInit(): void {
   }
